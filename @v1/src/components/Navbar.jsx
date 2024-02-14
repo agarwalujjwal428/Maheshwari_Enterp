5288ef76d3c5f18bf088524b716bf195966eac4c
agarwalujjwal428 <agarwalujjwal428@gmail.com>
2024-02-14 16:25:09 +0530

    w

--- @v1/src/components/Navbar.jsx
+++ @v1/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           <ul className="nav ">
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Ho
+                Home
               </a>
             </li>
             <li className="nav-item">
@@ -27,7 +27,7 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Bl
+                Blog
               </a>
             </li>
             <li className="nav-item">
