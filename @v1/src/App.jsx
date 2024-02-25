52145bda99887e3e5fd4ae1f037284df93094d2d
VaibhavG2598 <vaibhavgupta2598@gmail.com>
2024-02-25 16:19:11 +0530

    navbar updates

--- @v1/src/App.jsx
+++ @v1/src/App.jsx
@@ -11,9 +11,9 @@ const App = () => {
       <Navbar />
       <Section_2 />
       <Testimonials />
-      <Contactus/>
+      <Contactus />
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3325395304414!2d-122.01116148467422!3d37.33463524513264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb59127ce078f%3A0x18e1c3ce7becf1b!2sApple%20Park!5e0!3m2!1sen!2sin!4v1637309850935!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3578.1686840703283!2d72.98113219999999!3d26.256188200000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39418ebcd41ad64d%3A0xa7ebbd0f6f68247d!2sMaheshwari%20Enterprises!5e0!3m2!1sen!2sin!4v1708801277996!5m2!1sen!2sin"
         width="600"
         height="450"
         style={{ border: "0" }}
