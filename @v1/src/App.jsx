fdaebc7e78c878fac901c1a1505d3e0ad8d2558c
VaibhavG2598 <vaibhavgupta2598@gmail.com>
2024-02-26 17:54:41 +0530

    homepage changes

--- @v1/src/App.jsx
+++ @v1/src/App.jsx
@@ -5,16 +5,19 @@ import Section_2 from "./components/Section_2";
 import Testimonials from "./components/Testimonials";
 import Contactus from "./components/Contactus";
 import Aboutus from "./components/Aboutus";
+import Home_Bg from "./components/Home_Bg";
+
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Aboutus/>
+      <Home_Bg />
+      <Aboutus />
       <Section_2 />
       <Testimonials />
-      <Contactus/>
-    
+      <Contactus />
+
       <Footer />
     </>
   );
---
+++ @v1/src/components/Home_Bg.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Home_Bg_carousel from "./Home_Bg_carousel";
+import "../styles/Home_Bg.css";
+
+const Home_Bg = () => {
+  const slides = [
+    {
+      image: "/src/assets/images/3.jpg",
+      alt: "Slide 1",
+      caption: "This is slide 1",
+    },
+    {
+      image: "/src/assets/images/4.jpg",
+      alt: "Slide 2",
+      caption: "This is slide 2",
+    },
+    {
+      image: "/src/assets/images/5.jpg",
+      alt: "Slide 3",
+      caption: "This is slide 3",
+    },
+  ];
+
+  return (
+    <div className="home_bg_container">
+      <Home_Bg_carousel slides={slides} />
+      <div className="home_bg_header">
+        Maheshwari Enterprises
+      </div>
+    </div>
+  );
+};
+
+export default Home_Bg;
