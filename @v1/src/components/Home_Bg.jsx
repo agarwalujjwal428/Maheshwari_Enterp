68cbae04613c9002ae2290271cc3eb9cd7af2eb0
VaibhavG2598 <vaibhavgupta2598@gmail.com>
2024-03-07 13:01:57 +0530

    some color changes

--- @v1/src/components/Home_Bg.jsx
+++ @v1/src/components/Home_Bg.jsx
@@ -5,28 +5,26 @@ import "../styles/Home_Bg.css";
 const Home_Bg = () => {
   const slides = [
     {
-      image: "/src/assets/images/3.jpg",
+      image: "/src/assets/images/pic1.jpg",
       alt: "Slide 1",
       caption: "This is slide 1",
     },
     {
-      image: "/src/assets/images/4.jpg",
+      image: "/src/assets/images/pic2.jpg",
       alt: "Slide 2",
       caption: "This is slide 2",
     },
-    {
-      image: "/src/assets/images/5.jpg",
-      alt: "Slide 3",
-      caption: "This is slide 3",
-    },
+    // {
+    //   image: "/src/assets/images/pic3.webp",
+    //   alt: "Slide 3",
+    //   caption: "This is slide 3",
+    // },
   ];
 
   return (
     <div className="home_bg_container">
       <Home_Bg_carousel slides={slides} />
-      <div className="home_bg_header">
-        Maheshwari Enterprises
-      </div>
+      <div className="home_bg_header">Maheshwari Enterprises</div>
     </div>
   );
 };
