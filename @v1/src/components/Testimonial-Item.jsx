9e1d02b140dd965d91d314a4fe5a91a07b8f31e7
agarwalujjwal428 <agarwalujjwal428@gmail.com>
2024-04-25 14:57:54 +0530

    home_edit

--- @v1/src/components/Testimonial-Item.jsx
+++ @v1/src/components/Testimonial-Item.jsx
@@ -2,14 +2,14 @@ const Test_Item = ({ title, stat, icon }) => {
   return (
     <div className="funfact-single text-center">
       <div className="funfact-img">
-        <img src={icon}></img>
-        </div>
-        <div className="funfact-count text-uppercase">
-            <h3>
-                <span class="counter">{stat}</span>
-            </h3>
-            <h5>{title}</h5>
-        </div>
+        <img src={icon} style={{ width: "41px", height: "41px" }}></img>
+      </div>
+      <div className="funfact-count text-uppercase">
+        <h3>
+          <span class="counter">{stat} </span>
+        </h3>
+        <h5>{title}</h5>
+      </div>
     </div>
   );
 };
