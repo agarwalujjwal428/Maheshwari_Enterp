523886ff82ba2fc7ed967ff43b4aa1e76dd32881
VaibhavG2598 <vaibhavgupta2598@gmail.com>
2024-04-28 17:46:25 +0530

    semi-final

--- @v1/src/components/RpDetails.jsx
+++ @v1/src/components/RpDetails.jsx
@@ -1,8 +1,7 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import recentData from "../RecentProject.json";
 import Carousel from "react-bootstrap/Carousel";
-
 
 const RpDetails = () => {
   const { id, title, category } = useParams();
@@ -12,11 +11,10 @@ const RpDetails = () => {
   );
 
   useEffect(() => {
-    window.scrollTo(0,80); // Scroll to the top of the window when the component mounts
+    window.scrollTo(0, 80); // Scroll to the top of the window when the component mounts
   }, []); // Empty dependency array ensures the effect runs only once
 
   console.log("***recent project", rpItem);
-
 
   return (
     <>
@@ -27,26 +25,8 @@ const RpDetails = () => {
           <Carousel style={{ margin: "auto", height: "500px" }}>
             {rpItem.readMore.servItems.map((item, index) => (
               <Carousel.Item key={index} interval={3000}>
-                <img
-                  src={item["item-img"]}
-                  alt={item["item-desc"]}
-                  style={{
-                    height: "300px",
-                    width: "auto",
-                    margin: "60px 0 0 165px",
-                  }}
-                />
-                <p
-                  style={{
-                    color: "#606060",
-                    fontWeight: "bold",
-                    margin: "25px 0 20px 65px",
-                    width: "80%",
-                    textAlign: "justify",
-                  }}
-                >
-                  {item["item-desc"]}
-                </p>
+                <img src={item["item-img"]} alt={item["item-desc"]} />
+                <p style={{color:"#606060"}}>{item["item-desc"]}</p>
               </Carousel.Item>
             ))}
           </Carousel>
